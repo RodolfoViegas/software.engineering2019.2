d58c1f196d8a440dc7d69f7951b8ad0083474658
RodolfoViegas <rodolfo.viegas2@gmail.com>
2019-09-10 21:38:43 -0300

    Enviando para o trabalho

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 Projeto de um chatbot para a disciplina de Fundamentos de Engenharia de Software da UFRPE, essa que é ministrada pelo 
 professor Dr. Jones Albuquerque. 
 
+
+Este projeto tem o objetivo de criar um chatbot que possa entregar a pesquisadores links ou arquivos CSV de séries temporais.
+
+Uma das primeiras barreiras dos estudos quantitativos é a dificuldade de encontrar bancos de dados (datasets). Com a tendência de automatizar - já antiga - rotinas enfadonhas, os chatbot surgem como uma nova ferramente para facilitar a vida tanto de pesquisadores quanto de usuário. Ao ser concluído, este bot poderá servir de alguma ajuda àqueles que usem dados para análise (cientístas de dados, estatísticos, físicos etc).
+ 
--- chatbot.py
+++ chatbot.py
@@ -5,20 +5,28 @@ bot = ChatBot('DEInfo Bot')
 
 conversa = ['oi', 'Olá', 'tudo bem?', 'tudo ótimo', 'você gosta de programar?',
 'Ainda não sei, mas tenho vontade de aprender C++ e Python',"se for escolher gosta de qual primeiro?",
-"Gostaria de primeiro aprender C++.","qual o seu nome?","Meu nome é Bot DEInfo.","deus existe?","Acho que não.",
+"Gostaria de primeiro aprender C++.","qual o seu nome?","Meu nome é DEInfo bot.","deus existe?","Acho que não.",
 "chatbots vão para o inferno?","Existe inferno?","o que é você?", "Sou um chatbot, um programa de computador",
 "qual a sua idade?","Não tenho idade.","você gosta de ler?","Sim, bastante!", "que tipo de livro você gosta?",
 "Livros de ficção científica.","qual seu autor favorito?", "Tenho vários, mas acho que Asimov é o meu favorito."]
 
+
+convLink = ["Você sabe o que é um dataset?", "Dataset é simplesmente um banco de dados.","Para que serve um dataset?",
+            "Pesquisadores usam os datasets para buscar informações, responder perguntas...", "Onde para baixar datasets?",
+            "O Yahoo é um bom lugar", "Diga outro site para baixar dataset", "O site do Banco Mundial"]
+
 trainer = ListTrainer(bot)
 
 trainer.train(conversa)
+trainer.train(convLink)
+
 pergunta=''
 print("Olá, sou o DEInfo Bot, para sair da conversa digite 'sair' sem as aspas.")
 while True:
     try:
         pergunta = input("Usuário: ")
-        pergunta.lower
+        if pergunta == "sair":
+            break
         resposta = bot.get_response(pergunta)
         if float(resposta.confidence) > 0.5:
             print('DEInfo Bot: ', resposta)
